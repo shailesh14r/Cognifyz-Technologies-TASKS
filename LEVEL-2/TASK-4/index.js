ed2cea12c694c7063e93223945d46bd758db6147
Mona Lisa <mangalshailesh100@gmail.com>
2024-07-10 14:17:06 +0530

    Commit

--- LEVEL-2/TASK-4/index.js
+++ LEVEL-2/TASK-4/index.js
@@ -1,4 +1,4 @@
-function changeBackgroundColor() {
+function changeBackgroundColor(){
     const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = randomColor;
 }
